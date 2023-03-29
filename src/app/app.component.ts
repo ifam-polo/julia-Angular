b5c8a1f67fbbf15469d92918cb2d527f4f01331c
Julia <donadellij@gmail.com>
2023-03-29 11:14:14 -0400

    Entendendo input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,36 @@ import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, Afte
   selector: 'app-root',
   template: `
   <!--
-    <app-diretivas-estruturais></app-diretivas-estruturais>-->
-    <app-diretivas-atributos></app-diretivas-atributos>
+    <app-diretivas-estruturais></app-diretivas-estruturais>
+    <app-diretivas-atributos>
+    <h1>Aula de diretivas de atributo</h1>
+    <hr>
+    </app-diretivas-atributos>-->
+
+    <app-input [contador]="addValue"></app-input>
+    <br>
+    <button (click)="add()">Add</button>
+    <button (click)="subtrair()">Subtrair</button>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
 
+  public addValue: number = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  public add(){
+    this.addValue+=1;
+    console.log('somado')
+  }
+
+  public subtrair() {
+    this.addValue-=1
+  }
 
 
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+
+
+
+@NgModule({
+  declarations: [NewComponentComponent, InputComponent, OutputComponent],
+  exports:[NewComponentComponent, InputComponent], 
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
