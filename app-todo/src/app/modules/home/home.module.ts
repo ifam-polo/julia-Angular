d6b8e8aad87b7a64cd3ded1c11a4bb0d3c6b016a
Julia <donadellij@gmail.com>
2023-03-30 12:24:33 -0400

    iniciando projeto em Angular-- To Do List

---
+++ app-todo/src/app/modules/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//page
+import { HomeComponent } from './pages/home/home.component';
+
+//Components
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent, TodoButtonDeleteAllComponent, TodoInputAddItensComponent, TodoListComponent, HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
