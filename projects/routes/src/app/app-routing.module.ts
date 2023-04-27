8d3b66ccec360e0c4d43c70163d7d3d6028ee0f6
Julia <donadellij@gmail.com>
2023-04-27 11:28:30 -0400

    Entendendo rotas ativas, parametros de rotas, navageção entre paginas, redirecionamneto e rotas filhas

--- projects/routes/src/app/app-routing.module.ts
+++ projects/routes/src/app/app-routing.module.ts
@@ -8,7 +8,9 @@ import { SobreComponent } from './pages/sobre/sobre.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent, pathMatch: 'full'},
-  {path: "sobre", component: SobreComponent},
+  {path: "sobre", component: SobreComponent, children: [
+    {path: "julia", component: SobreComponent},
+  ]},
   {path: "404", component: PageErrorComponent},
   {path: "**",  redirectTo: '404' } // rota coringa- quando a rota n existe
 
--- projects/routes/src/app/pages/sobre/sobre.component.ts
+++ projects/routes/src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
+ 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit{
+
+  constructor(private activatedRoute:ActivatedRoute,
+    private router: Router
+    ){}
+
+  ngOnInit(): void {
+    
+    this.activatedRoute.params.subscribe({
+      next: (res: any) => console.log(res.id, res.username),
+      error: (err) => console.log(err)
+    })
+
+    this.activatedRoute.queryParams.subscribe({
+      next: (res: any) => console.log(res.id, res.username),
+      error: (err) => console.log(err)
+    })
+
+    setInterval( ()=> { 
+      //this.router.navigate(['404']); //funciona tipo link. Usa normamente em paginas internas
+      this.router.navigateByUrl('404');// faz o reload da pagina
+    }, 5000)
+
+    
+  }
 
 }
