02603e97dba0cdae018eae4e16572deb559d8621
Julia <donadellij@gmail.com>
2023-05-02 11:11:03 -0400

    Iniciando projeto pokedex, criação de componentes, modules e um pouco de estilização.

---
+++ projects/pokedex/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//Modules
+import { PagesModule } from './pages/pages.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+//COmponents
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
