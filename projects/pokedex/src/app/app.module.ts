a61242c357a742dedea60413d7be80227ca0263d
Julia <donadellij@gmail.com>
2023-05-03 12:33:00 -0400

    Estilização da página principal e utilização da pokeAPI.

--- projects/pokedex/src/app/app.module.ts
+++ projects/pokedex/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 //Modules
 import { PagesModule } from './pages/pages.module';
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
