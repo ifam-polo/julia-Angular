2b169ff0933fbc881dc6f0c47753835d77cc09e5
Julia <donadellij@gmail.com>
2023-05-04 12:16:21 -0400

    Criação da função para o método get. Introdução do método map e tap do RxJS

--- projects/pokedex/src/app/service/poke-api.service.ts
+++ projects/pokedex/src/app/service/poke-api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http'
 
 //observable
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
   
 
 
@@ -18,12 +18,25 @@ export class PokeApiService {
     private http: HttpClient
   ) { }
 
-  get apiListAllPokemons(): Observable<any>{
+  get apiListAllPokemons():Observable<any>{
     return this.http.get<any>(this.url).pipe(
-      tap(res => res),
+      tap( res => res ),
       tap( res => {
-        console.log(res)
+        res.results.map( (resPokemons: any) => {
+          this.apiGetPokemon(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          );
+
+        })
       })
+    )
+  }
+
+  public apiGetPokemon( url: string ):Observable<any>{
+    return this.http.get<any>( url ).pipe(
+      map(
+        res => res
+      )
     )
   }
 }
