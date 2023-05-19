4128c3a6cad58895cca991d97664ca2863a85804
Julia <donadellij@gmail.com>
2023-05-19 15:17:29 -0400

    Criação de componentes, modules e estilização da página principal e da página após o login

--- projetoLogin/login/src/app/app-routing.module.ts
+++ projetoLogin/login/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { core } from '@angular/compiler';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=> 
+      import('./core/components/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>  
+      import('./components/admin/admin.module').then( m => m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
