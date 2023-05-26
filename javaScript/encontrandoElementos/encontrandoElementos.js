cdfae2d6e63dc74a413280d8864cb9b07f9ef29d
julia <donadellij@gmail.com>
2023-05-26 11:48:27 -0400

    Encontrando elementos do HTML com Javascript. Instalação do node e angular

---
+++ javaScript/encontrandoElementos/encontrandoElementos.js
@@ -0,0 +1,10 @@
+"use strict"
+
+//encontrando o pai
+
+
+//const strong = document.querySelector("strong");
+let p = document.querySelector(".paragrafo");
+
+//console.log(strong.parentElement);
+console.log(p.parentNode);
