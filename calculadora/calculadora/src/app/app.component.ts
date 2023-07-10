f7b03532dd60b82faede0994aa97e2be13ffb5e1
Julia <donadellij@gmail.com>
2023-07-10 13:03:44 -0300

    Preparação da tela da calculadora com tailwindCss

---
+++ calculadora/calculadora/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calculadora';
+  //parei no minuto 25:05 https://www.youtube.com/watch?v=NWZniJF9zX4
+}
