00d1e9bcf23f698fcce59cc3783dd6fe54e4263c
Julia <donadellij@gmail.com>
2023-07-12 12:52:50 -0300

    Inicio de um crud com Angular reactive forms, angular material UI e json-server

---
+++ material-crud/ang-material/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ang-material';
+
+  //https://www.youtube.com/watch?v=jGbP620NahE&t=731s
+}
