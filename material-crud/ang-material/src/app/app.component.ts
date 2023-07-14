a04af70bd33c81330904c8a63f67ffd497e5c0c3
julia <donadellij@gmail.com>
2023-07-14 12:26:09 -0400

    Continuação do projetos -> criação de alguns componentes e lógica e continuação da estilização da página

--- material-crud/ang-material/src/app/app.component.ts
+++ material-crud/ang-material/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ang-material';
 
-  //https://www.youtube.com/watch?v=jGbP620NahE&t=731s
+  constructor(private dialog: MatDialog){
+
+  }
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width:'30%'
+    });
+  }
 }
+
